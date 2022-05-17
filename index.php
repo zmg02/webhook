ea9fcfb52c7869cbbb56d712b2160a10d95b52be
zmg <z20200602@outlook.com>
2022-05-17 15:37:07 +0800

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+ini_set("error_reporting", "E_ALL & ~E_NOTICE");
+
+echo "Hi，Webhooks！By 小傅哥<br/>";
+
+echo "<br/><a href='https://www.aliyun.com/minisite/goods?taskPkg=1111ydsrwb&pkgSid=11388&recordId=1033318&userCode=is4kfbdt'>新用户购买服务器优惠活动🔥</a>";
+
+echo '<br/>测试：输出项目路径和用户目录：<br/>';
+
+exec("cd ~ && cd - && cd -", $output);
+
+echo '<pre>';
+echo print_r($output);
+echo '</pre>';
---
+++ webhooks.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Git webhooks 自动部署脚本
+ * 地址：https://github.com/fuzhengwei/guide-webhooks/settings/hooks
+ */
+
+// 接收post参数
+$requestBody = file_get_contents("php://input");
+if (empty($requestBody)) {
+    exit('data null！');
+}
+
+// Content type = application/json
+$content = json_decode($requestBody, true);
+
+// 验证 Webhooks 配置的 Secret，也可以不验证
+/*if (empty($content['password']) || $content['password'] != '123456') {
+	exit('password error');
+}*/
+
+// 项目存放物理路径，也就是站点的访问地址
+$path = "/www/wwwroot/39.96.73.167/guide-webhooks/";
+
+// 判断需要下拉的分支上是否有提交，我们这里的分支名称为 main
+if ($content['ref'] == 'refs/heads/main') {
+
+    // 执行脚本 git pull，拉取分支最新代码
+    $res = shell_exec("cd {$path} && git pull origin main 2>&1"); // 当前为www用户
+
+    // 记录日志 ($content 返回的是一整个对象，可以按需获取里面的内容，写入日志)
+    $res_log = '------------------------->' . PHP_EOL;
+    $res_log .= '用户 ' . $content['pusher']['name'] . ' 于 ' . date('Y-m-d H:i:s') . ' 向项目【' . $content['repository']['name'] . '】分支【' . $content['ref'] . '】PUSH ' . $content['commits'][0]['message'] . PHP_EOL;
+    $res_log .= $res . PHP_EOL;
+
+    // 追加方式，写入日志文件
+    file_put_contents("git_webhook_log.txt", $res_log, FILE_APPEND);
+}
+echo 'done';
+
