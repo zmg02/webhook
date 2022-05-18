f47c1dc045158d2e403bcce0dd72d2aecd6f375a
zmg <z20200602@outlook.com>
2022-05-18 11:40:31 +0800

    webhook测试

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ ini_set("error_reporting", "E_ALL & ~E_NOTICE");
 echo "Hi，Webhooks！By 小傅哥<br/>";
 
 echo "<br/><a href='https://www.aliyun.com/minisite/goods?taskPkg=1111ydsrwb&pkgSid=11388&recordId=1033318&userCode=is4kfbdt'>新用户购买服务器优惠活动🔥</a>";
+echo "webhook 测试！";
 
 echo '<br/>测试：输出项目路径和用户目录：<br/>';
 
